579a797c8295376bb472c79ed4dd32afdae73485
front-thinking <269354528@qq.com>
2017-08-12 20:48:05 +0800

    enable remove watcher & ttl & etc.

--- src/scope.js
+++ src/scope.js
@@ -1,22 +1,84 @@
 'use strict';
 var _ = require('lodash');
 
-function Scope() {
-    this.$$watchers = [];
+function initWatchVal() {
 }
 
-Scope.prototype.$watch = function(watchFn, listenerFn) {
+function Scope() {
+    this.$$watchers = [];
+    this.$$lastDirtyWatch = null;
+}
+
+Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
+    var self = this;
     var watcher = {
         watchFn: watchFn,
-        listenerFn: listenerFn
+        listenerFn: listenerFn || function () {
+        },
+        valueEq: !!valueEq,
+        last: initWatchVal
     };
-    this.$$watchers.push(watcher);
+    this.$$watchers.unshift(watcher);
+    this.$$lastDirtyWatch = null;
+    return function () {
+        var index = self.$$watchers.indexOf(watcher);
+        if (index >= 0) {
+            self.$$watchers.splice(index, 1);
+            self.$$lastDirtyWatch = null;
+        }
+    }
 };
 
-Scope.prototype.$digest = function() {
-    _.forEach(this.$$watchers, function(watcher) {
-        watcher.listenerFn();
+// 一旦有变化，则返回true
+Scope.prototype.$$digestOnce = function () {
+    var self = this;
+    var newValue, oldValue, dirty;
+    _.forEachRight(this.$$watchers, function (watcher) {
+        try {
+            if (watcher) {
+                newValue = watcher.watchFn(self);
+                oldValue = watcher.last;
+                if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+                    self.$$lastDirtyWatch = watcher;
+                    watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+                    // 第二个参数做一下判断，如果是初试第一次执行，则将新值作为旧值传到监听函数里，避免将initWatchVal函数暴漏出去
+                    watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), self);
+
+                    dirty = true;
+                } else if (self.$$lastDirtyWatch === watcher) {
+                    return false;
+                }
+            }
+        } catch (e) {
+            console.error(e);
+        }
+
     });
+
+    return dirty;
+};
+
+Scope.prototype.$digest = function () {
+    var ttl = 10;
+    var dirty;
+    this.$$lastDirtyWatch = null;
+    do {
+        dirty = this.$$digestOnce();
+        if (dirty && !(ttl--)) {
+            throw '10 digest iterations reached';
+        }
+    } while (dirty);
+};
+
+Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
+    if (valueEq) {
+        return _.isEqual(newValue, oldValue);
+    } else {
+        return newValue === oldValue ||
+            (typeof newValue === 'number' && typeof oldValue === 'number' &&
+            isNaN(newValue) && isNaN(oldValue));
+    }
+    ;
 };
 
 module.exports = Scope;
