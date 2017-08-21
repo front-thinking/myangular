cb1225da0e8d2b37fa29e9b26bed7264ff55bdd6
front-thinking <269354528@qq.com>
2017-08-21 23:45:17 +0800

    parse interger setup

---
+++ src/parse.js
@@ -0,0 +1,61 @@
+'use strict';
+
+function parse(expr) {
+    var lexer = new Lexer();
+    var parser = new Parser(lexer);
+    return parser.parse(expr);
+}
+
+
+/*
+ * Lexer
+ * */
+function Lexer() {
+}
+
+Lexer.prototype.lex = function (text) {
+    // Tokenization will be done here
+};
+
+
+/*
+ * AST
+ * */
+function AST(lexer) {
+    this.lexer = lexer;
+}
+
+AST.prototype.ast = function (text) {
+    this.tokens = this.lexer.lex(text);
+    // AST building will be done here
+};
+
+
+/*
+ * ASTCompiler
+ * */
+function ASTCompiler(astBuilder) {
+    this.astBuilder = astBuilder;
+}
+
+ASTCompiler.prototype.compile = function (text) {
+    var ast = this.astBuilder.ast(text);
+    // AST compilation will be done here
+};
+
+
+/*
+ * Parser
+ * */
+function Parser(lexer) {
+    this.lexer = lexer;
+    this.ast = new AST(this.lexer);
+    this.astCompiler = new ASTCompiler(this.ast);
+}
+
+Parser.prototype.parse = function (text) {
+    return this.astCompiler.compile(text);
+};
+
+
+module.exports = parse;
