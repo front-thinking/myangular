c0feed6873618d82b2498be9774029d0042ff4f7
front-thinking <269354528@qq.com>
2017-08-27 22:44:26 +0800

    finish Non-Computed Attribute Lookup

--- src/parse.js
+++ src/parse.js
@@ -27,7 +27,7 @@ Lexer.prototype.lex = function (text) {
             this.readNumber();
         } else if (this.is('\'"')) {
             this.readString(this.ch);
-        } else if (this.is('[],{}:')) {
+        } else if (this.is('[],{}:.')) {
             this.tokens.push({
                 text: this.ch
             });
@@ -180,6 +180,7 @@ AST.ObjectExpression = 'ObjectExpression';
 AST.Property = 'Property';
 AST.Identifier = 'Identifier';
 AST.ThisExpression = 'ThisExpression';
+AST.MemberExpression = 'MemberExpression';
 
 AST.prototype.ast = function (text) {
     this.tokens = this.lexer.lex(text);
@@ -191,17 +192,28 @@ AST.prototype.program = function () {
 };
 
 AST.prototype.primary = function () {
+    var primary;
     if (this.expect('[')) {
-        return this.arrayDeclaration();
+        primary = this.arrayDeclaration();
     } else if (this.expect('{')) {
-        return this.object();
+        primary = this.object();
     } else if (this.constants.hasOwnProperty(this.tokens[0].text)) {
-        return this.constants[this.consume().text];
+        primary = this.constants[this.consume().text];
     } else if (this.peek().identifier) {
-        return this.identifier();
+        primary = this.identifier();
     } else {
-        return this.constant();
+        primary = this.constant();
     }
+
+    while (this.expect('.')) {
+        primary = {
+            type: AST.MemberExpression,
+            object: primary,
+            property: this.identifier()
+        };
+    }
+
+    return primary;
 };
 
 AST.prototype.expect = function (e) {
@@ -298,6 +310,7 @@ ASTCompiler.prototype.compile = function (text) {
 };
 
 ASTCompiler.prototype.recurse = function (ast) {
+    var intoId;
     switch (ast.type) {
         case AST.Program:
             this.state.body.push('return ', this.recurse(ast.body), ';');
@@ -319,11 +332,17 @@ ASTCompiler.prototype.recurse = function (ast) {
             }, this));
             return '{' + properties.join(',') + '}';
         case AST.Identifier:
-            var intoId = this.nextId();
+            intoId = this.nextId();
             this.if_('s', this.assign(intoId, this.nonComputedMember('s', ast.name)));
             return intoId;
         case AST.ThisExpression:
             return 's';
+        case AST.MemberExpression:
+            intoId = this.nextId();
+            var left = this.recurse(ast.object);
+            this.if_(left,
+                this.assign(intoId, this.nonComputedMember(left, ast.property.name)));
+            return intoId;
     }
 
 };
