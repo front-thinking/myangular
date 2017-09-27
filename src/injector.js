f8eb88d6c7c17eec628acf990eaad8fb0af88e03
front-thinking <269354528@qq.com>
2017-09-27 17:15:07 +0800

    finish DI

--- src/injector.js
+++ src/injector.js
@@ -15,6 +15,17 @@ function createInjector(modulesToLoad) {
         }
     };
 
+    function invoke(fn) {
+        var args = _.map(fn.$inject, function(token) {
+            if (_.isString(token)) {
+                return cache[token];
+            } else {
+                throw 'Incorrect injection token! Expected a string, got '+token;
+            }
+        });
+        return fn.apply(null, args);
+    }
+
     _.forEach(modulesToLoad, function loadModule(moduleName) {
         if (!loadedModules.hasOwnProperty(moduleName)) {
             loadedModules[moduleName] = true;
@@ -34,7 +45,8 @@ function createInjector(modulesToLoad) {
         },
         get: function (key) {
             return cache[key];
-        }
+        },
+        invoke: invoke
     };
 }
 
