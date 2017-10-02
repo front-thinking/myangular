db534bdf5ab99243f4d50717a0e39366a32f01c5
front-thinking <269354528@qq.com>
2017-10-02 22:28:10 +0800

    before refactor every old code module

---
+++ src/angular_public.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var setupModuleLoader = require('./loader');
+
+function publishExternalAPI() {
+
+    setupModuleLoader(window);
+
+    var ngModule = window.angular.module('ng', []);
+
+}
+
+module.exports = publishExternalAPI;
