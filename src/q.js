314c2828a48078bb83d92be84077c3094f6db0cb
front-thinking <269354528@qq.com>
2017-10-06 00:12:54 +0800

    finish promise chaining

--- src/q.js
+++ src/q.js
@@ -11,11 +11,13 @@ function $QProvider() {
         }
 
         Promise.prototype.then = function (onFulfilled, onRejected) {
+            var result = new Deferred();
             this.$$state.pending = this.$$state.pending || [];
-            this.$$state.pending.push([null, onFulfilled, onRejected]);
+            this.$$state.pending.push([result, onFulfilled, onRejected]);
             if (this.$$state.status > 0) {
                 scheduleProcessQueue(this.$$state);
             }
+            return result.promise;
         };
 
         Promise.prototype.catch = function (onRejected) {
@@ -62,9 +64,14 @@ function $QProvider() {
             var pending = state.pending;
             state.pending = undefined;
             _.forEach(pending, function (handlers) {
+                var deferred = handlers[0];
                 var fn = handlers[state.status];
                 if (_.isFunction(fn)) {
-                    fn(state.value);
+                    deferred.resolve(fn(state.value));
+                } else if (state.status === 1) {
+                    deferred.resolve(state.value);
+                } else {
+                    deferred.reject(state.value);
                 }
             });
         }
