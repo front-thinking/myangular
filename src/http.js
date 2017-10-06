f70a14292ce44e80fdf77643f7312557d5ccbdc4
front-thinking <269354528@qq.com>
2017-10-06 23:59:50 +0800

    finish Sending HTTP Requests

--- src/http.js
+++ src/http.js
@@ -2,10 +2,37 @@
 
 function $HttpProvider() {
 
-    this.$get = ['$httpBackend', function ($httpBackend) {
+    this.$get = ['$httpBackend', '$q', '$rootScope',
+        function ($httpBackend, $q, $rootScope) {
 
-    }];
+            return function $http(config) {
 
+                var deferred = $q.defer();
+
+                function done(status, response, statusText) {
+                    status = Math.max(status, 0);
+                    deferred[isSuccess(status) ? 'resolve' : 'reject']({
+                        status: status,
+                        data: response,
+                        statusText: statusText,
+                        config: config
+                    });
+
+                    if (!$rootScope.$$phase) {
+                        $rootScope.$apply();
+                    }
+                }
+
+                $httpBackend(config.method, config.url, config.data, done);
+                return deferred.promise;
+
+            };
+
+        }];
+}
+
+function isSuccess(status) {
+    return status >= 200 && status < 300;
 }
 
 module.exports = $HttpProvider;
