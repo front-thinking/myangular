226b9cc50f89774fbd4b60bad3239dce858ee2f6
front-thinking <269354528@qq.com>
2017-10-06 23:10:30 +0800

    finish chapter14

--- src/angular_public.js
+++ src/angular_public.js
@@ -10,7 +10,8 @@ function publishExternalAPI() {
     ngModule.provider('$filter', require('./filter'));
     ngModule.provider('$parse', require('./parse'));
     ngModule.provider('$rootScope', require('./scope'));
-    ngModule.provider('$q', require('./q'));
+    ngModule.provider('$q', require('./q').$QProvider);
+    ngModule.provider('$$q', require('./q').$$QProvider);
 
 
 }
