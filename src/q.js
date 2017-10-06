d82bae4cb407ee53513bc594b5b7e51d803564ca
front-thinking <269354528@qq.com>
2017-10-06 21:55:18 +0800

    finish Immediate Rejection - $q.reject

--- src/q.js
+++ src/q.js
@@ -132,8 +132,15 @@ function $QProvider() {
             return new Deferred();
         }
 
+        function reject(rejection) {
+            var d = defer();
+            d.reject(rejection);
+            return d.promise;
+        }
+
         return {
-            defer: defer
+            defer: defer,
+            reject: reject
         };
 
     }];
