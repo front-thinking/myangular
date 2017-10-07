5563978d3d032f775f989ac9c9d7c2fcb6f92c6d
front-thinking <269354528@qq.com>
2017-10-07 19:32:23 +0800

    finish Pending Requests

--- src/http.js
+++ src/http.js
@@ -151,6 +151,12 @@ function $HttpProvider() {
 
             function sendReq(config, reqData) {
                 var deferred = $q.defer();
+                $http.pendingRequests.push(config);
+                deferred.promise.then(function () {
+                    _.remove($http.pendingRequests, config);
+                }, function () {
+                    _.remove($http.pendingRequests, config);
+                });
 
                 function done(status, response, headersString, statusText) {
                     status = Math.max(status, 0);
@@ -266,6 +272,7 @@ function $HttpProvider() {
             }
 
             $http.defaults = defaults;
+            $http.pendingRequests = [];
 
             _.forEach(['get', 'head', 'delete'], function (method) {
                 $http[method] = function (url, config) {
