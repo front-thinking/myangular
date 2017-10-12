a32e2360d313a8458970b201e376d7fd01e1fdee
front-thinking <269354528@qq.com>
2017-10-12 19:45:12 +0800

    finish Directive Link Functions

--- test/compile_spec.js
+++ test/compile_spec.js
@@ -1034,22 +1034,44 @@ describe('$compile', function () {
         });
 
 
-        describe('linking', function() {
+        describe('linking', function () {
 
-            it('takes a scope and attaches it to elements', function() {
-                var injector = makeInjectorWithDirectives('myDirective', function() {
+            it('takes a scope and attaches it to elements', function () {
+                var injector = makeInjectorWithDirectives('myDirective', function () {
                     return {compile: _.noop};
                 });
-                injector.invoke(function($compile, $rootScope) {
+                injector.invoke(function ($compile, $rootScope) {
                     var el = $('<div my-directive></div>');
                     $compile(el)($rootScope);
                     expect(el.data('$scope')).toBe($rootScope);
                 });
             });
 
+            it('calls directive link function with scope', function () {
+                var givenScope, givenElement, givenAttrs;
+                var injector = makeInjectorWithDirectives('myDirective', function () {
+                    return {
+                        compile: function () {
+                            return function link(scope, element, attrs) {
+                                givenScope = scope;
+                                givenElement = element;
+                                givenAttrs = attrs;
+                            };
+                        }
+                    };
+                });
+                injector.invoke(function ($compile, $rootScope) {
+                    var el = $('<div my-directive></div>');
+                    $compile(el)($rootScope);
+                    expect(givenScope).toBe($rootScope);
+                    expect(givenElement[0]).toBe(el[0]);
+                    expect(givenAttrs).toBeDefined();
+                    expect(givenAttrs.myDirective).toBeDefined();
+                });
+            });
+
 
         });
-
 
 
     });
