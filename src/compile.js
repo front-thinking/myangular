6d2912808807db54aef8e5897897d9287cb4631d
front-thinking <269354528@qq.com>
2017-10-17 23:19:55 +0800

    finish two-way data binding

--- src/compile.js
+++ src/compile.js
@@ -381,6 +381,7 @@ function $CompileProvider($provide) {
                         $element.data('$isolateScope', isolateScope);
                         _.forEach(newIsolateScopeDirective.$$isolateBindings, function (definition, scopeName) {
                             var attrName = definition.attrName;
+                            var parentGet, unwatch;
                             switch (definition.mode) {
                                 case '@':
                                     attrs.$observe(attrName, function (newAttrValue) {
@@ -394,11 +395,37 @@ function $CompileProvider($provide) {
                                     if (definition.optional && !attrs[attrName]) {
                                         break;
                                     }
-                                    var parentGet = $parse(attrs[attrName]);
+                                    parentGet = $parse(attrs[attrName]);
                                     isolateScope[scopeName] = parentGet(scope);
-                                    var unwatch = scope.$watch(parentGet, function (newValue) {
+                                    unwatch = scope.$watch(parentGet, function (newValue) {
                                         isolateScope[scopeName] = newValue;
                                     });
+                                    isolateScope.$on('$destroy', unwatch);
+                                    break;
+                                case '=':
+                                    if (definition.optional && !attrs[attrName]) {
+                                        break;
+                                    }
+                                    parentGet = $parse(attrs[attrName]);
+                                    var lastValue = isolateScope[scopeName] = parentGet(scope);
+                                    var parentValueWatch = function () {
+                                        var parentValue = parentGet(scope);
+                                        if (isolateScope[scopeName] !== parentValue) {
+                                            if (parentValue !== lastValue) {
+                                                isolateScope[scopeName] = parentValue;
+                                            } else {
+                                                parentValue = isolateScope[scopeName];
+                                                parentGet.assign(scope, parentValue);
+                                            }
+                                        }
+                                        lastValue = parentValue;
+                                        return lastValue;
+                                    };
+                                    if (definition.collection) {
+                                        unwatch = scope.$watchCollection(attrs[attrName], parentValueWatch);
+                                    } else {
+                                        unwatch = scope.$watch(parentValueWatch);
+                                    }
                                     isolateScope.$on('$destroy', unwatch);
                                     break;
                             }
@@ -434,11 +461,12 @@ var PREFIX_REGEXP = /(x[\:\-_]|data[\:\-_])/i;
 function parseIsolateBindings(scope) {
     var bindings = {};
     _.forEach(scope, function (definition, scopeName) {
-        var match = definition.match(/\s*([@<])(\??)\s*(\w*)\s*/);
+        var match = definition.match(/\s*([@<]|=(\*?))(\??)\s*(\w*)\s*/);
         bindings[scopeName] = {
-            mode: match[1],
-            optional: match[2],
-            attrName: match[3] || scopeName
+            mode: match[1][0],
+            collection: match[2] === '*',
+            optional: match[3],
+            attrName: match[4] || scopeName
         };
     });
     return bindings;
