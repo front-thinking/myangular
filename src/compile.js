6bd4c9c0228151ee853c5967b4c48dfd2bcd23cc
front-thinking <269354528@qq.com>
2017-10-17 20:17:46 +0800

    finish Isolate Attribute Bindings

--- src/compile.js
+++ src/compile.js
@@ -23,6 +23,9 @@ function $CompileProvider($provide) {
                         if (directive.link && !directive.compile) {
                             directive.compile = _.constant(directive.link);
                         }
+                        if (_.isObject(directive.scope)) {
+                            directive.$$isolateBindings = parseIsolateBindings(directive.scope);
+                        }
                         directive.name = directive.name || name;
                         directive.index = i;
                         return directive;
@@ -375,6 +378,19 @@ function $CompileProvider($provide) {
                     isolateScope = scope.$new(true);
                     $element.addClass('ng-isolate-scope');
                     $element.data('$isolateScope', isolateScope);
+                    _.forEach(newIsolateScopeDirective.$$isolateBindings, function (definition, scopeName) {
+                        var attrName = definition.attrName;
+                        switch (definition.mode) {
+                            case '@':
+                                attrs.$observe(attrName, function(newAttrValue) {
+                                    isolateScope[scopeName] = newAttrValue;
+                                });
+                                if (attrs[attrName]) {
+                                    isolateScope[scopeName] = attrs[attrName];
+                                }
+                                break;
+                        }
+                    });
                 }
 
                 _.forEach(preLinkFns, function (linkFn) {
@@ -402,6 +418,18 @@ $CompileProvider.$inject = ['$provide'];
 
 
 var PREFIX_REGEXP = /(x[\:\-_]|data[\:\-_])/i;
+
+function parseIsolateBindings(scope) {
+    var bindings = {};
+    _.forEach(scope, function (definition, scopeName) {
+        var match = definition.match(/\s*@\s*(\w*)\s*/);
+        bindings[scopeName] = {
+            mode: '@',
+            attrName: match[1] || scopeName
+        };
+    });
+    return bindings;
+}
 
 function directiveNormalize(name) {
     return _.camelCase(name.replace(PREFIX_REGEXP, ''));
