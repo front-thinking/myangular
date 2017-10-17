618ddc1daa3162d48d49fceefd170bb336afc5b9
front-thinking <269354528@qq.com>
2017-10-17 16:52:49 +0800

    finish Pre- And Post-Linking

--- src/compile.js
+++ src/compile.js
@@ -286,7 +286,7 @@ function $CompileProvider($provide) {
             var $compileNode = $(compileNode);
             var terminalPriority = -Number.MAX_VALUE;
             var terminal = false;
-            var linkFns = [];
+            var preLinkFns = [], postLinkFns = [];
             _.forEach(directives, function (directive) {
                 if (directive.$$start) {
                     $compileNode = groupScan(compileNode, directive.$$start, directive.$$end);
@@ -298,8 +298,15 @@ function $CompileProvider($provide) {
 
                 if (directive.compile) {
                     var linkFn = directive.compile($compileNode, attrs);
-                    if (linkFn) {
-                        linkFns.push(linkFn);
+                    if (_.isFunction(linkFn)) {
+                        postLinkFns.push(linkFn);
+                    } else if (linkFn) {
+                        if (linkFn.pre) {
+                            preLinkFns.push(linkFn.pre);
+                        }
+                        if (linkFn.post) {
+                            postLinkFns.push(linkFn.post);
+                        }
                     }
                 }
                 if (directive.terminal) {
@@ -309,11 +316,14 @@ function $CompileProvider($provide) {
             });
 
             function nodeLinkFn(childLinkFn, scope, linkNode) {
+                var $element = $(linkNode);
+                _.forEach(preLinkFns, function (linkFn) {
+                    linkFn(scope, $element, attrs);
+                });
                 if (childLinkFn) {
                     childLinkFn(scope, linkNode.childNodes);
                 }
-                _.forEach(linkFns, function (linkFn) {
-                    var $element = $(linkNode);
+                _.forEachRight(postLinkFns, function (linkFn) {
                     linkFn(scope, $element, attrs);
                 });
             }
